423836ca7d4d7b06eebbe18236703a452ee7a6bf
kindom <1034218685@qq.com>
2020-10-13 10:57:29 +0800

    first commit

---
+++ Example/Pods/Target Support Files/LYNetworking/LYNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LYNetworking : NSObject
+@end
+@implementation PodsDummy_LYNetworking
+@end
---
+++ LYNetworking/Classes/LYNetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  LYNetworkManager.swift
+//  LYNetworking_Example
+//
+//  Created by 赵赵 on 2020/10/13.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class LYNetworkManager: NSObject {
+    
+    func testPrint(text:String) {
+        print("LYNetworkManager print ==" + text)
+    }
+}
